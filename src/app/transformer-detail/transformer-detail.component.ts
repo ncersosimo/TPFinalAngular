5144ae3726c37b452153b56dba8c756d4a0b6050
Nicolas Cersosimo <ncersosimo@gmail.com>
2024-09-08 19:14:13 -0300

    Corrijo

--- src/app/transformer-detail/transformer-detail.component.ts
+++ src/app/transformer-detail/transformer-detail.component.ts
@@ -12,25 +12,22 @@ import { GoBackComponent } from '../go-back/go-back.component';
   template: `
   @let transformer = (transformer$ | async);
   <div class="container mt-1">
-    <!-- Contenedor principal con imagen -->
     <div class="row justify-content-center mb-4">
-      <!-- Columna para la imagen -->
       <div class="col-md-6 text-center">
         <img [src]="transformer?.avatar" alt="Transformer Image" class="img-fluid rounded">
       </div>
     </div>
 
-    <!-- Fila para los detalles del transformer -->
     <div class="row justify-content-center">
       <div class="col-md-8">
         <div class="card border-light mb-3">
           <div class="card-body text-center">
             @if(!transformer){
-              <p>Cargando...</p> <!-- Mensaje de carga mientras se obtienen los datos -->
+              <p>Loading...</p>
             }@else {
-              <h5 class="card-title">{{transformer.name}}</h5> <!-- Nombre del transformer -->
-              <p class="card-text"><strong>Tipo:</strong> {{transformer.type}}</p> <!-- Tipo del transformer -->
-              <p class="card-text"><strong>Detalles:</strong> {{transformer.details}}</p> <!-- Detalles del transformer -->
+              <h5 class="card-title">{{transformer.name}}</h5>
+              <p class="card-text"><strong>Tipo:</strong> {{transformer.type}}</p>
+              <p class="card-text"><strong>Detalles:</strong> {{transformer.details}}</p>
             }
           </div>
         </div>
